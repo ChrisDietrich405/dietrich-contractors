7f9020c0ba804b89b22aaeebd6f26f115cc6034c
Chris <chrisdietrich3666@gmail.com>
2024-07-09 21:04:40 -0400

    feat improved contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -19,16 +19,16 @@ const Contact = () => {
 
   const firstNameRef = useRef(null);
   const lastNameRef = useRef(null);
-  // const emailRef = useRef(null);
-  // const phoneRef = useRef(null);
-  // const messageRef = useRef(null);
+  const emailRef = useRef(null);
+  const phoneRef = useRef(null);
+  const messageRef = useRef(null);
 
   const clearFormFields = () => {
     firstNameRef.current.value = "";
     lastNameRef.current.value = "";
-    // emailRef.current.value = "";
-    // phoneRef.current.value = "";
-    // messageRef.current.value = "";
+    emailRef.current.value = "";
+    phoneRef.current.value = "";
+    messageRef.current.value = "";
   };
 
   const sendEmail = async (e) => {
@@ -39,23 +39,23 @@ const Contact = () => {
         {
           firstName: firstNameRef.current.value,
           lastName: lastNameRef.current.value,
-          // email: emailRef.current.value,
-          // phone: phoneRef.current.value,
-          // message: messageRef.current.value,
+          email: emailRef.current.value,
+          phone: phoneRef.current.value,
+          message: messageRef.current.value,
         },
         {
           firstName: "required",
           lastName: "required",
-          // email: "required|email",
-          // phone: "required|email",
-          // message: "required",
+          email: "required|email",
+          phone: "required|email",
+          message: "required",
         },
         {
           "required.firstName": "The first name field is required",
           "required.lastName": "The last name field is required",
-          // "required.email": "The email field is required",
-          // "required.phone": "The phone field is required",
-          // "required.message": "The message field is required",
+          "required.email": "The email field is required",
+          "required.phone": "The phone field is required",
+          "required.message": "The message field is required",
         }
       );
       console.log("hello");
@@ -63,10 +63,10 @@ const Contact = () => {
         const templateParams = {
           firstName: firstNameRef.current.value,
           lastName: lastNameRef.current.value,
-          // email: emailRef.current.value,
-          // phone: phone.current.value,
-          // message: messageRef.current.value,
-          // to_name: "Dietrich Contractors",
+          email: emailRef.current.value,
+          phone: phone.current.value,
+          message: messageRef.current.value,
+          to_name: "Dietrich Contractors",
         };
 
         await emailjs.send(
@@ -96,23 +96,32 @@ const Contact = () => {
       <Typography variant="h4" mb={2} mt={2} sx={{ textAlign: "center" }}>
         Contact us today for a free estimate
       </Typography>
-      <Grid container spacing={2}>
+      <Grid
+        onSubmit={sendEmail}
+        container
+        spacing={2}
+        sx={{
+          border: "1px solid black",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
         <Grid item md={6} xs={12}>
           <Box
             component="form"
             sx={{
-              "& > :not(style)": { m: 1, width: "50%" },
+              borderRadius: "6px",
+              padding: "10px 30px",
             }}
-            onSubmit={sendEmail}
           >
-            <label htmlFor="firstName">First Name</label>
+            {/* <label htmlFor="firstName">First Name</label> */}
             <input
               id="firstName"
               type="text"
               ref={firstNameRef}
               name="firstName"
               placeholder="First Name"
-              // className={styles.input_text}
+              className={styles.input_text}
               tabIndex="1"
             />
             {errors.firstName && (
@@ -121,64 +130,70 @@ const Contact = () => {
               </p>
             )}
 
-            <label htmlFor="lastName">Last Name</label>
+            {/* <label htmlFor="lastName">Last Name</label> */}
             <input
               id="lastName"
               type="text"
               ref={lastNameRef}
               name="lastName"
               placeholder="Last Name"
-              // className={styles.input_text}
+              className={styles.input_text}
               tabIndex="1"
             />
-            {errors.firstName && (
+            {errors.lastName && (
               <p className={styles.error_message}>
                 {errors.lastName.join(", ")}
               </p>
             )}
 
-            {/* <TextField
-              id="lastName"
-              label="Last Name"
-              variant="outlined"
-              onChange={(e) => setLastName(e.target.value)}
-              type="text"
-            ></TextField>
-            {errors.firstName && (
-              <p className={styles.error_message}>
-                {errors.lastName.join(", ")}
-              </p>
-            )} */}
-
-            {/* <TextField
+            <input
               id="email"
-              label="Email"
-              variant="outlined"
-              onChange={(e) => setEmail(e.target.value)}
               type="text"
-            ></TextField>
-
-            <TextField
+              ref={emailRef}
+              name="email"
+              placeholder="email"
+              className={styles.input_text}
+              tabIndex="1"
+            />
+            {errors.email && (
+              <p className={styles.error_message}>{errors.email.join(", ")}</p>
+            )}
+            <input
               id="phone"
-              label="Phone"
-              variant="outlined"
-              onChange={(e) => setPhone(e.target.value)}
               type="text"
-            ></TextField>
-
-            <TextField
-              id="message"
-              label="Message"
-              variant="outlined"
-              onChange={(e) => setMessage(e.target.value)}
-              multiline
-              rows={5}
-            /> */}
-            <Button type="submit">Submit</Button>
+              ref={phoneRef}
+              name="phone"
+              placeholder="phone number"
+              className={styles.input_text}
+              tabIndex="1"
+            />
+            {errors.phone && (
+              <p className={styles.error_message}>{errors.phone.join(", ")}</p>
+            )}
+            <Button
+              sx={{ marginTop: "20px", width: "100%", textAlign: "center" }}
+              type="submit"
+            >
+              Submit
+            </Button>
           </Box>
         </Grid>
-        <Grid item xs={6}>
+        <Grid item md={6} xs={6}>
           <Box>
+            <textarea
+              id="message"
+              placeholder="Start typing..."
+              className={styles.message}
+              name="message"
+              ref={messageRef}
+            />
+            {errors.message && (
+              <p className={styles.error_message}>
+                {errors.message.join(", ")}
+              </p>
+            )}
+          </Box>
+          {/* <Box>
             <Image
               style={{ borderRadius: "6px" }}
               src="/fence.jpg"
@@ -186,7 +201,7 @@ const Contact = () => {
               height={300}
               alt="Picture of the author"
             />
-          </Box>
+          </Box> */}
         </Grid>
       </Grid>
     </Container>
